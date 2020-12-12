2e408e2920f840cd63e4c7d238c7cb6f63133531
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-12-12 16:21:45 +0100

    feat: redirect from home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,51 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import Button from '../components/Button';
 import Layout from '../components/Layout';
+import * as texts from '../utils/translation';
 
 export default function Home() {
+  let history = useHistory();
   return (
     <Layout>
-      <form className="mt-8 space-y-6" action="#" method="POST">
-        <input type="hidden" name="remember" value="true" />
-        <div className="-space-y-px rounded-md shadow-sm">
-          <div>
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="relative block w-full px-3 py-2 border rounded-none appearance-none text-neutral-900 placeholder-neutral-500 border-neutral-300 rounded-t-md focus:outline-none focus:ring-primary-800 focus:border-primary-800 focus:z-10 sm:text-sm"
-              placeholder="Email address"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="sr-only">
-              Password
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="relative block w-full px-3 py-2 border rounded-none appearance-none text-neutral-900 placeholder-neutral-500 border-neutral-300 rounded-b-md focus:outline-none focus:ring-primary-800 focus:border-primary-800 focus:z-10 sm:text-sm"
-              placeholder="Password"
-            />
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md bg-primary-800 group hover:bg-primary-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-800"
-          >
-            Sign in
-          </button>
-        </div>
-      </form>
+      <div className="flex justify-between">
+        <Button
+          text={texts.es.booking}
+          onClick={() => history.push('/booking')}
+        />
+        <Button
+          text={texts.es.verify}
+          onClick={() => history.push('/verify')}
+        />
+        <Button
+          text={texts.es.cancel}
+          onClick={() => history.push('/cancel')}
+        />
+      </div>
     </Layout>
   );
 }
